28072dcb7be7c56f79e31b0971b1bbb9644c95ca
Inaxo <90970323+Inaxo@users.noreply.github.com>
2023-05-04 13:48:19 +0200

    Enhance NumbersLogic class with new number theory functions

--- NumbersLogic/Primes.h
+++ NumbersLogic/Primes.h
@@ -16,11 +16,13 @@ namespace MathStuff {
             static int largestTwinPrime(int n);
             static int sumOfPrimes(int n);
             static int productOfPrimes(int n);
+            static bool isCircular(int n);
             static bool isCircularPrime(int n);
             static int kthPrimeFactor(int n, int k);
             static bool isEmirp(int n);
             static int sumOfNPrimes(int n);
             static int productOfNPrimes(int n);
+            static std::vector<int> primeFactorization(int n);
         
     };
 }
