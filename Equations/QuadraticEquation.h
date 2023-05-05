96c3e7997a31132cbbdae751d9ec9d3f886c21da
Inaxo <90970323+Inaxo@users.noreply.github.com>
2023-05-05 19:24:07 +0200

    Refactor project structure to use additional files for improved organization part-2

---
+++ Equations/QuadraticEquation.h
@@ -0,0 +1,22 @@
+#include "../MathLib.h"
+namespace MathStuff
+{
+    class QuadraticEquation
+    {
+    public:
+        //Functions returning information about the quadratic equation
+        static bool hasRealRoots(double a, double b, double c);
+        static int numberOfRoots(double a, double b, double c);
+        static std::vector<double> roots(double a, double b, double c);
+        static std::pair<double, double> getVertex(double a, double b, double c);
+        static double getAxisOfSymmetry(double a, double b);
+        static double getDiscriminant(double a, double b, double c);
+        static bool isConcaveUp(double a);
+        static bool isConcaveDown(double a);
+        static double getLatusRectum(double a);
+        static std::pair<double, double> getFocus(double a, double b, double c);
+        static double getDirectrix(double a, double b, double c);
+        static std::string getEquation(double a, double b, double c);
+
+    };
+}
