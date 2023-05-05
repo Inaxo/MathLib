c8731004ec8741ae87d04bfbde3027678f706bf1
Inaxo <90970323+Inaxo@users.noreply.github.com>
2023-05-05 19:22:44 +0200

    Refactor project structure to use additional files for improved organization

--- MathLib.h
+++ MathLib.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <vector>
+//Operators overloading f. definitions [Supported operations: add,substract,divide,multiply]
 template <typename T, template <typename, typename> class Container>
 Container<T, std::allocator<T>> operator-(const Container<T, std::allocator<T>>& c1, const Container<T, std::allocator<T>>& c2);
 template <typename T, template <typename, typename> class Container>
@@ -17,24 +18,7 @@ namespace MathStuff
 
     class Arithmetic;
     class NumbersLogic;
-    class QuadraticEquation
-    {
-    public:
-        //Functions returning information about the quadratic equation
-        static bool hasRealRoots(double a, double b, double c);
-        static int numberOfRoots(double a, double b, double c);
-        static std::vector<double> roots(double a, double b, double c);
-        static std::pair<double, double> getVertex(double a, double b, double c);
-        static double getAxisOfSymmetry(double a, double b);
-        static double getDiscriminant(double a, double b, double c);
-        static bool isConcaveUp(double a);
-        static bool isConcaveDown(double a);
-        static double getLatusRectum(double a);
-        static std::pair<double, double> getFocus(double a, double b, double c);
-        static double getDirectrix(double a, double b, double c);
-        static std::string getEquation(double a, double b, double c);
-
-    };
+    class QuadraticEquation;
 
     class SearchingAlgorithms
     {
